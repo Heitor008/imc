201fc71a88b5754d1f715d211205e04ae9ff46e5
Heitor008 <roheitor27@gmail.com>
2025-09-07 11:48:07 -0300

    Add files to upload

---
+++ imc.js
@@ -0,0 +1,9 @@
+
+function calcular(){
+    let peso = Number(document.getElementById('txtp').value)
+    let altura = Number(document.getElementById('txta').value)/100
+    let imc = peso / ( altura * altura)
+
+    document.getElementById('res').innerText = `O seu Índice de massa corpórea é de ${imc.toFixed(2)}`
+       
+}
